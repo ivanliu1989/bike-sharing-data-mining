ff78c8f0550c9aefe90037eaf342efbfda36eea4
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-08-26 16:18:25 +1000

    random forest

--- dmbk.R
+++ dmbk.R
@@ -1,3 +1,25 @@
 setwd(choose.dir())
-train <- read.csv('Data/train.csv', head=T)
-test <- read.csv('Data/test.csv', head=T)
+train <- read.csv('Data/train.csv', head=T, stringsAsFactor=F)
+test <- read.csv('Data/test.csv', head=T, stringsAsFactor=F)
+str(train)
+str(test)
+
+require(lubridate)
+require(randomForest)
+
+train$hour <- hour(train$datetime)
+test$hour <- hour(test$datetime)
+head(train)
+
+train$wd <- wday(train$datetime)
+test$wd <- wday(test$datetime)
+
+test$count <- 0
+
+fit <- randomForest(as.factor(count) ~ season + holiday + weather + wd+ hour + temp + atemp
+                    + humidity + windspeed , data=train, ntree = 700, importance=TRUE)
+varImpPlot(fit)
+
+pred <- predict(fit, test)
+submit <- data.frame(datetime=test$datetime, count=pred)
+write.csv(submit, file='rf.csv',row.names=F)
