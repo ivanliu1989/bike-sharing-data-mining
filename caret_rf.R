74f45fa59eb72c5f29c6b76bf603be0afe893685
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-09-14 14:05:32 +1000

    model

--- caret_rf.R
+++ caret_rf.R
@@ -1,7 +1,6 @@
 setwd("C:\\Users\\Ivan.Liuyanfeng\\Desktop\\Data_Mining_Work_Space\\bike-sharing-data-mining")
-require(Metrics)
+gc()
 require(lubridate)
-require(randomForest)
 require(caret)
 # preprocessing
 train <- read.csv('Data/train.csv', head=T, stringsAsFactor=F)
@@ -11,20 +10,49 @@ test$hour <- hour(test$datetime)
 train$wd <- wday(train$datetime)
 test$wd <- wday(test$datetime)
 test$count <- 0
+head(test)
+head(train)
+train <- train[,-1]
+train <- train[,-10]
+train <- train[,-9]
+train$season <- as.factor(train$season)
+train$holiday <- as.factor(train$holiday)
+train$workingday <- as.factor(train$workingday)
+train$weather <- as.factor(train$weather)
+train$hour <- as.factor(train$hour)
+train$wd <- as.factor(train$wd)
+test$season <- as.factor(test$season)
+test$holiday <- as.factor(test$holiday)
+test$workingday <- as.factor(test$workingday)
+test$weather <- as.factor(test$weather)
+test$hour <- as.factor(test$hour)
+test$wd <- as.factor(test$wd)
+
 
 # parameter tuning
-fitControl <- trainControl(method='repeatedcv', # 10-fold CV
-                           number = 10, 
-                           repeats =10) # repeated ten times
+set.seed(1234)  
+fitControl <- trainControl(method='cv', # 10-fold CV
+                           10) # repeated ten times
+Grid <-  expand.grid(
+    n.trees = c(1000),
+    interaction.depth = c(22) ,
+    shrinkage = 0.2)
 # boosted tree model
-gbmFit1 <- train(count ~ season + holiday + weather + wd+ hour + temp + atemp
-                 + humidity + windspeed,
-                 data=train, method='rf', trControl = fitControl, verbose=F)
+gbmFit1 <- train(as.factor(count) ~ ., data=train, method='gbm', trControl = fitControl, verbose=T,
+                 tuneGrid = Grid)
 gbmFit1
-nrow(test)
-trellis.par.set(caretTheme())
-plot(gbmFit1)
-predictions <- predict(gbmFit1, test)
+pred1 <- predict(gbmFit1, train)
+confusionMatrix(pred1, as.integer(train$count))
+
+# fit 2 poisson
+fit2 <- train(as.factor(count) ~ ., data=train, method='glm', verbose=T, family='poisson')
+
+# random forest
+fit3 <- train(as.factor(count) ~ . , data=train, method='rf', verbose=T, trControl=fitControl)
+
+# predictions
+predictions <- predict(fit2, test)
 gbmPrinted <- data.frame(test$datetime, predictions)
 names(gbmPrinted)<- c('datetime', 'count')
-write.table(x=gbmPrinted, file='gbm.csv', sep=',', row.names=F)
+gbmPrinted[which(gbmPrinted$count < 0),'count'] <- 0
+write.table(x=gbmPrinted, file='gbm_cv.csv', sep=',', row.names=F)
